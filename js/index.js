e6b0132055dca43302ab300a4948880e3bf8a700
auevangel <auevangel@gmail.com>
2022-02-07 00:34:13 +1100

    Fixing play button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,33 @@
-function playAudio(audioId) { 
-        let x = document.getElementById(audioId);
-\       x.play();
-};
+   
+   function playAudio(audioId) {
+            let audioElement = document.getElementById(audioId);
+            audioElement.play();
+           };
+    
+
+
+
+    function pauseAudio(audioId) {
+            let audioElement = document.getElementById(audioId);
+            audioElement.pause();
+        };
+
+    function move(audioId, barId) {
+              let durationSeconds = document.getElementById(audioId).duration;
+            let totalLength = durationSeconds * 1000;
+            let elem = document.getElementById(barId);
+            let id = setInterval(frame, 1);
+                function frame() {
+                    let currentPosition = document.getElementById(audioId).currentTime;
+                    let currentMicroseconds = currentPosition * 1000;
+                    let rawWidth = (currentMicroseconds / totalLength) * 100;
+                    let width = Math.round(rawWidth) / 100;
+                    if (width >= 100) {
+                            clearInterval(id);
+                            i = 0;
+                            } else {
+                                elem.style.width = width + "%";
+                            }
+                    }
+                }
+        };
